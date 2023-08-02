be2477335ad5e27e15d2c721056a84f183951efb
Tatsuo Yamashita <yto@ytosmbpm1.local>
2023-08-03 08:15:57 +0900

    add furigana api sample

---
+++ chex-sample-furigana/content-script.js
@@ -0,0 +1,58 @@
+const APPID = 'gUxspU.xg66pvU6W5OJMz0vH10FYB.FT4sWcQomZrtmPD6sG.14VlAuMdCGoBuIeMyOpRtlJBlc-';
+
+async function yapifuri(query) {
+    const url = "https://jlp.yahooapis.jp/FuriganaService/V2/furigana" + "?appid=" + encodeURIComponent(APPID);
+    const res = await fetch(url, {
+        method: 'POST', header: {'Content-Type': 'application/json'}, mode: 'cors',
+        body: JSON.stringify({
+            "id": "A123", "jsonrpc" : "2.0", "method" : "jlp.furiganaservice.furigana",
+            "params" : { "q" : query, "grade" : 1 }
+        }),
+    });
+    return res.json();
+}
+
+async function proc_selection() {
+    const e = document.querySelector("#selected_text");
+    if (e) {
+        e.innerHTML = e.innerHTML.replace(/<rt>.*?<\/rt>/g,'');
+        let j = await yapifuri(e.textContent);
+        if (j['result']['word']) {
+            e.innerHTML = j['result']['word'].map(x =>
+                x['furigana'] ? "<ruby>" + x['surface'] + "<rt>" + x['furigana'] + "</rt></ruby>" : x['surface']
+            ).join("");
+            unwrap_selection();
+        }
+    }
+}
+
+document.onmouseup = function() {
+    const selection = document.getSelection();
+    if (selection.isCollapsed) return;
+    const range = selection.getRangeAt(0);
+    if (range.collapsed) return;
+    
+    unwrap_selection();
+    
+    const selected_text = document.createElement('span');
+    selected_text.id = "selected_text";
+    selected_text.style.backgroundColor = 'pink';
+    selected_text.onclick = (event) => unwrap_selection();
+    range.surroundContents(selected_text);
+    
+    const btn = document.createElement('span');
+    btn.id = "button_selected_text";
+    btn.style.padding = "0 0.5em";
+    btn.style.backgroundColor = "red";
+    btn.onclick = (event) => proc_selection();
+    selected_text.after(btn);
+};
+
+function unwrap_selection() {
+    const e = document.querySelector("#selected_text");
+    if (!e) return;
+    while (e.firstChild) e.parentNode.insertBefore(e.firstChild, e); // unwrap
+    e.remove();
+    document.querySelector("#button_selected_text").remove()
+}
+
