a7befb5f34d5cf27610d8157eb55e9632cbd1a74
Tatsuo Yamashita <yto@ytosmbpm1.local>
2023-08-05 18:06:03 +0900

    fix furigana

--- chex-sample-furigana/content-script.js
+++ chex-sample-furigana/content-script.js
@@ -1,5 +1,4 @@
 const APPID = 'gUxspU.xg66pvU6W5OJMz0vH10FYB.FT4sWcQomZrtmPD6sG.14VlAuMdCGoBuIeMyOpRtlJBlc-';
-
 async function yapifuri(query) {
     const url = "https://jlp.yahooapis.jp/FuriganaService/V2/furigana" + "?appid=" + encodeURIComponent(APPID);
     const res = await fetch(url, {
@@ -14,16 +13,16 @@ async function yapifuri(query) {
 
 async function proc_selection() {
     const e = document.querySelector("#selected_text");
-    if (e) {
-        e.innerHTML = e.innerHTML.replace(/<rt>.*?<\/rt>/g,'');
-        let j = await yapifuri(e.textContent);
-        if (j['result']['word']) {
-            e.innerHTML = j['result']['word'].map(x =>
-                x['furigana'] ? "<ruby>" + x['surface'] + "<rt>" + x['furigana'] + "</rt></ruby>" : x['surface']
-            ).join("");
-            unwrap_selection();
-        }
-    }
+    if (! e) return;
+    e.innerHTML = e.innerHTML.replace(/<rt>.*?<\/rt>/g,'');
+    let j = await yapifuri(e.textContent);
+    if (! j['result']['word']) return;
+    e.innerHTML = j['result']['word'].map(x =>
+        x['furigana'] ?
+            "<ruby>" + x['surface'] + "<rt>" + x['furigana'] + "</rt></ruby>" :
+            x['surface']
+    ).join("");
+    unwrap_selection();
 }
 
 document.onmouseup = function() {
@@ -31,15 +30,15 @@ document.onmouseup = function() {
     if (selection.isCollapsed) return;
     const range = selection.getRangeAt(0);
     if (range.collapsed) return;
-    
+
     unwrap_selection();
-    
+
     const selected_text = document.createElement('span');
     selected_text.id = "selected_text";
     selected_text.style.backgroundColor = 'pink';
     selected_text.onclick = (event) => unwrap_selection();
     range.surroundContents(selected_text);
-    
+
     const btn = document.createElement('span');
     btn.id = "button_selected_text";
     btn.style.padding = "0 0.5em";
@@ -52,7 +51,7 @@ document.onmouseup = function() {
 function unwrap_selection() {
     const e = document.querySelector("#selected_text");
     if (!e) return;
-    while (e.firstChild) e.parentNode.insertBefore(e.firstChild, e); // unwrap
+    while (e.firstChild) e.parentNode.insertBefore(e.firstChild, e);
     e.remove();
     document.querySelector("#button_selected_text").remove()
 }
